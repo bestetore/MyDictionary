b793f27369a440c39eb0769d88bbe2e196678f05
Beste Töre <bestetore.98@gmail.com>
2021-02-27 19:10:59 +0300

    Add project files.

---
+++ MyDictionary/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> myDictionary = new MyDictionary<string>();
+            myDictionary.Add("Melek");
+            myDictionary.Add("Ayşe");
+            myDictionary.Add("Emine");
+            myDictionary.Add("Fatma");
+            myDictionary.Add("Ali");
+            myDictionary.Add("Cın");
+            myDictionary.Add("Biti");
+
+
+            foreach (var item in myDictionary.Items)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
+
+
